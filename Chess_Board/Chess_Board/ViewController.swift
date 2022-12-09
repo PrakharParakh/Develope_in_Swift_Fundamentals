897203f702a58c99c46c1ea995bc5834df0de3db
Prakhar Parakh <93584013+PrakharParakh@users.noreply.github.com>
2022-12-09 13:46:32 +0530

    Add files via upload

---
+++ Chess_Board/Chess_Board/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  Chess_Board
+//
+//  Created by admin on 04/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    @IBOutlet weak var queenImage: UIImageView!
+    @IBOutlet weak var kingimage: UIImageView!
+    @IBOutlet weak var knightImage: UIImageView!
+    @IBOutlet weak var bishopImage: UIImageView!
+    @IBOutlet weak var pawnImage: UIImageView!
+    @IBOutlet weak var rookImage: UIImageView!
+    
+    @IBOutlet weak var queenLabel: UILabel!
+    @IBOutlet weak var kingLabel: UILabel!
+    @IBOutlet weak var knightLabel: UILabel!
+    @IBOutlet weak var bishopLabel: UILabel!
+    @IBOutlet weak var pawnLabel: UILabel!
+    @IBOutlet weak var rookLabel: UILabel!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
